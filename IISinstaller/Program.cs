10bcdd55e8b85a8c01e29efe819df3ee08a7b7ce
Wasim Akhtar <wasim.akh2@gmail.com>
2019-05-13 02:29:19 +0530

    a

--- IISinstaller/Program.cs
+++ IISinstaller/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Microsoft.Web.Administration;
 
@@ -18,21 +19,21 @@ namespace IISinstaller
             //to check for its existence by name
             if (server.ApplicationPools != null && server.ApplicationPools.Count > 0)
             {
-                if (server.ApplicationPools.Where(p => p.Name == "MyApplicationPool").Count()>0)
+                if (server.ApplicationPools.Where(p => p.Name == "sms.local").Count()>0)
                 {
                     //if we find the pool already there, we will get a referecne to it for update
-                    myApplicationPool = server.ApplicationPools.FirstOrDefault(p => p.Name == "MyApplicationPool");
+                    myApplicationPool = server.ApplicationPools.FirstOrDefault(p => p.Name == "sms.local");
                 }
                 else
                 {
                     //if the pool is not already there we will create it
-                    myApplicationPool = server.ApplicationPools.Add("MyApplicationPool");
+                    myApplicationPool = server.ApplicationPools.Add("sms.local");
                 }
             }
             else
             {
                 //if the pool is not already there we will create it
-                myApplicationPool = server.ApplicationPools.Add("MyApplicationPool");
+                myApplicationPool = server.ApplicationPools.Add("sms.local");
             }
 
             if (myApplicationPool != null)
@@ -41,7 +42,7 @@ namespace IISinstaller
                 myApplicationPool.ProcessModel.IdentityType = ProcessModelIdentityType.NetworkService;
 
                 //we set the runtime version
-                myApplicationPool.ManagedRuntimeVersion = "v4.0";
+                myApplicationPool.ManagedRuntimeVersion = "No Managed Code";
 
                 //we save our new ApplicationPool!
                 server.CommitChanges();
@@ -50,10 +51,10 @@ namespace IISinstaller
             if (server.Sites != null && server.Sites.Count > 0)
             {
                 //we will first check to make sure that the site isn't already there
-                if (server.Sites.FirstOrDefault(s => s.Name == "MySite") == null)
+                if (server.Sites.FirstOrDefault(s => s.Name == "ncstcsmsoffline") == null)
                 {
                     //we will just pick an arbitrary location for the site
-                    string path = @"c:\MySiteFolder\";
+                    string path = @"C:\inetpub\wwwroot\ncstcsmslocal\";
 
                     //we must specify the Binding information
                     string ip = "*";
@@ -63,7 +64,7 @@ namespace IISinstaller
                     string bindingInfo = string.Format(@"{0}:{1}:{2}", ip, port, hostName);
 
                     //add the new Site to the Sites collection
-                    Site site = server.Sites.Add("MySite", "http", bindingInfo, path);
+                    Site site = server.Sites.Add("ncstcsmsoffline", "http", bindingInfo, path);
 
                     //set the ApplicationPool for the new Site
                     site.ApplicationDefaults.ApplicationPoolName = myApplicationPool.Name;
@@ -72,7 +73,39 @@ namespace IISinstaller
                     server.CommitChanges();
                 }
             }
+            string sourceDirectory = @"C:\Users\WasimSana\source\repos\SchoolManagementSystemTool\SchoolManagementSystemTool\bin\Release\netcoreapp2.2\";
+            string targetDirectory = @"C:\inetpub\wwwroot\ncstcsmslocal\";
 
+            Copy(sourceDirectory, targetDirectory);
+
+        }
+
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
         }
     }
 }
