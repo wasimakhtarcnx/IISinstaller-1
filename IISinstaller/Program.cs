623fa3e2fd80a0601d1afa1ed076114c4662541f
Wasim Akhtar <wasim.akh2@gmail.com>
2019-05-12 10:52:34 +0530

    Add project files.

---
+++ IISinstaller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IISinstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
